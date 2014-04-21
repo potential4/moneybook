ac12959ceef2cbb7b30c8c776a0b90848e321337
ysw505 <ysw5055@gmail.com>
2014-04-22 00:16:51 +0900

    widget -> activity 열기 기능 테스트 추가

--- MoneyBookProject/src/com/example/widgettest/TestWidget.java
+++ MoneyBookProject/src/com/example/widgettest/TestWidget.java
@@ -1,6 +1,7 @@
 package com.example.widgettest;
 
 
+import com.example.swipetest.MainActivity;
 import com.example.swipetest.R;
 
 import android.app.PendingIntent;
@@ -14,7 +15,11 @@ import android.util.Log;
 import android.widget.RemoteViews;
 
 public class TestWidget extends AppWidgetProvider {
-	 private static final String SYNC_CLICKED    = "automaticWidgetSyncButtonClick";
+	 private static final String BTN1_CLICKED    = "btn1Click";
+	 private static final String BTN2_CLICKED    = "btn2Click";
+	 private static final String BTN3_CLICKED    = "btn3Click";
+	 private static final String TEXTVIEW_CLICKED    = "textViewClick";
+
 
 	    @Override
 	    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -24,7 +29,14 @@ public class TestWidget extends AppWidgetProvider {
 	        remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
 	        watchWidget = new ComponentName(context, TestWidget.class);
 
-	        remoteViews.setOnClickPendingIntent(R.id.btn1, getPendingSelfIntent(context, SYNC_CLICKED));
+	        Intent activityIntent = new Intent(context, MainActivity.class);
+	        PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0, activityIntent, 0);
+
+	        remoteViews.setOnClickPendingIntent(R.id.targetText, configPendingIntent);	        
+	        remoteViews.setOnClickPendingIntent(R.id.btn1, getPendingSelfIntent(context, BTN1_CLICKED));
+	        remoteViews.setOnClickPendingIntent(R.id.btn2, getPendingSelfIntent(context, BTN2_CLICKED));
+	        remoteViews.setOnClickPendingIntent(R.id.btn3, getPendingSelfIntent(context, BTN3_CLICKED));
+	        	
 	        appWidgetManager.updateAppWidget(watchWidget, remoteViews);
 	    }
 
@@ -33,15 +45,15 @@ public class TestWidget extends AppWidgetProvider {
 	        // TODO Auto-generated method stub
 	        super.onReceive(context, intent);
 	        Log.i("recieve", intent.getAction());
-	        
-	        if( "android.appwidget.action.APPWIDGET_UPDATE".equals(intent.getAction())){
-	            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-	            RemoteViews remoteViews;
-	            ComponentName watchWidget;
+	        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            RemoteViews remoteViews;
+            ComponentName watchWidget;
 
-	            remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-	            watchWidget = new ComponentName(context, TestWidget.class);
-	            
+            remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+            watchWidget = new ComponentName(context, TestWidget.class);
+            
+	        if( "android.appwidget.action.APPWIDGET_UPDATE".equals(intent.getAction())){
+	        
 	            Bundle bdl = intent.getExtras();
 	            
 	            remoteViews.setTextViewText(R.id.targetText, (CharSequence) bdl.get("money"));
@@ -50,21 +62,28 @@ public class TestWidget extends AppWidgetProvider {
 
 	        }
 	        
-	        if (SYNC_CLICKED.equals(intent.getAction())) {
+	        if (BTN1_CLICKED.equals(intent.getAction())) {
 
-	            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-
-	            RemoteViews remoteViews;
-	            ComponentName watchWidget;
-
-	            remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-	            watchWidget = new ComponentName(context, TestWidget.class);
-	            
-	            remoteViews.setTextViewText(R.id.targetText, "TESTING");
+	            remoteViews.setTextViewText(R.id.targetText, "BTN1 CLICKED");
 
 	            appWidgetManager.updateAppWidget(watchWidget, remoteViews);
-
 	        }
+	        
+	        if (BTN2_CLICKED.equals(intent.getAction())) {
+
+	            
+	            remoteViews.setTextViewText(R.id.targetText, "BTN2 CLICKED");
+
+	            appWidgetManager.updateAppWidget(watchWidget, remoteViews);
+	        }
+	        
+	        if (BTN3_CLICKED.equals(intent.getAction())) {
+
+	            remoteViews.setTextViewText(R.id.targetText, "BTN3 CLICKED");
+
+	            appWidgetManager.updateAppWidget(watchWidget, remoteViews);
+	        }
+	   
 	    }
 
 	    protected PendingIntent getPendingSelfIntent(Context context, String action) {
